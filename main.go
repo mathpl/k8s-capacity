ab21ca0390080e9ab6caba53aa968a5a87f8e36b
Mathieu Payeur Levallois <mpl@jive.com>
2017-06-05 16:25:02 -0400

    support stdin if no Args

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"k8s.io/kubernetes/pkg/api/resource"
 )
 
 func main() {
-	qty, err := resource.ParseQuantity(os.Args[1])
+	var (
+		input string
+		err   error
+	)
+	if len(os.Args) != 2 {
+		reader := bufio.NewReader(os.Stdin)
+		input, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+			return
+		}
+	} else {
+		input = os.Args[1]
+	}
+	input = strings.Trim(input, "\n")
+	qty, err := resource.ParseQuantity(input)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 		return
 	}
-	i, ok := qty.AsInt64()
+	d := qty.AsDec()
+	i, ok := d.Unscaled()
 	if !ok {
 		fmt.Println("Unable to output as int64")
 		os.Exit(1)
